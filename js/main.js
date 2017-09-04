5094f474f67a7761b9ccd040ae542e03745864b8
Esteban Amas <esteban@bluestonelogic.com>
2017-09-04 09:20:51 -0400

    Adding some static data for Doughnut chart widget

--- js/main.js
+++ js/main.js
@@ -29,7 +29,28 @@ function redraw(animation){
     } else {
         options.animation = true;
     }
+    var data = [
+        {
+            value: 20,
+            color:"#637b85"
+        },
+        {
+            value : 30,
+            color : "#2c9c69"
+        },
+        {
+            value : 40,
+            color : "#dbba34"
+        },
+        {
+            value : 10,
+            color : "#c62f29"
+        }
 
+    ];
+    var canvas = document.getElementById("hours");
+    var ctx = canvas.getContext("2d");
+    new Chart(ctx).Doughnut(data, options);
 
 
 
