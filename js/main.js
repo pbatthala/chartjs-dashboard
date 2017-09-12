325d47ce71cba3d6241243f6b0a9dd5f8854717b
Esteban Amas <esteban@bluestonelogic.com>
2017-09-12 15:39:24 -0400

    Remove data button calls new function, larger bank of colors

--- js/main.js
+++ js/main.js
@@ -40,12 +40,18 @@ data = {
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
+               'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }],
@@ -73,12 +79,24 @@ var hours = new Chart(ctx, {
         addData(hours, makeid(), Math.floor(Math.random() * 20));
     });
 
+    $('#removeNewData').click(function(){
+        removeData(hours);
+    });
+
     function addData(chart, label, data) {
             chart.data.labels.push(label);
             chart.data.datasets.forEach((dataset) => {
                 dataset.data.push(data);
             });
             chart.update();
+    }
+
+    function removeData(chart) {
+            chart.data.labels.pop();
+            chart.data.datasets.forEach((dataset) => {
+            dataset.data.pop();
+        });
+        chart.update();
     }
 
     function makeid() {
