c4083a2efafc42f3a75635ea801f9586f75f1838
Esteban Amas <esteban@bluestonelogic.com>
2017-09-12 15:02:13 -0400

    Updated to ChartJS 2.7.0 usage patterns

--- js/main.js
+++ js/main.js
@@ -31,70 +31,115 @@
         }
 
     // --------------- DOUGHNUT GRAPH
-    var data = [
-    {
-        value: 20,
-        color:"#637b85"
-    },
-    {
-        value : 30,
-        color : "#2c9c69"
-    },
-    {
-        value : 40,
-        color : "#dbba34"
-    },
-    {
-        value : 10,
-        color : "#c62f29"
+
+data = {
+
+        datasets: [{
+            label: 'Breakdown: Appropriation Type',
+            data: [46, 17, 24],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }],
+        labels: ["RDTE", "OMMC", "PMC"]
+    };
+
+var ctx = document.getElementById("hours");
+
+var hours = new Chart(ctx, {
+    type: 'doughnut',
+    data: data
+});
+
+    // $('#ajax').click(function(){
+    //     // alert('ajax');
+    //      $.ajax({
+    //        // type: "GET",
+    //        url: "http://localhost:7070/GetAllProjects",
+    //        dataType: "json",
+    //        // data: data,
+    //        success: function(data){
+    //             var arraydata = JSON.parse(data);
+    //                 $.each(arraydata, function(index, el) {
+    //                 alert("element at " + index + ": " + el); // will alert each value
+    //             });
+    //        }
+    //     });
+    // });
+
+//     // --------------- BAR GRAPH
+    var barData = {
+            labels: ["C-1", "A-1", "A-2", "B-1"],
+            datasets: [{
+                label: "Label",
+                // xAxisID: "Contract",
+                // yAxisID: "Millions",
+                data: [17, 25, 30, 24],
+                backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
     }
 
-    ];
-    var canvas = document.getElementById("hours");
-    var ctx = canvas.getContext("2d");
-    new Chart(ctx).Doughnut(data, options);
+    var cty = document.getElementById("contracts");
+    var contracts = new Chart(cty, {
+        type: 'bar',
+        data: barData,
+    });
 
-    // --------------- LINE GRAPH
-    var data = {
-        labels : ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
-        datasets : [
-        {
-            fillColor : "rgba(99,123,133,0.4)",
-            strokeColor : "rgba(220,220,220,1)",
-            pointColor : "rgba(220,220,220,1)",
-            pointStrokeColor : "#fff",
-            data : [65,54,30,81,56,55,40]
-        },
-        {
-            fillColor : "rgba(219,186,52,0.4)",
-            strokeColor : "rgba(220,220,220,1)",
-            pointColor : "rgba(220,220,220,1)",
-            pointStrokeColor : "#fff",
-            data : [20,60,42,58,31,21,50]
-        },
-        ]
-    }
 
-    var canvas = document.getElementById("shipments");
-    var ctx = canvas.getContext("2d");
-    new Chart(ctx).Line(data, options);
+//     // --------------- RADAR GRAPH
 
-    // --------------- RADAR GRAPH
-    var data = {
-        labels : ["Helpful","Friendly","Kind","Rude","Slow","Frustrating"],
-        datasets : [
-        {
-            fillColor : "rgba(220,220,220,0.5)",
-            strokeColor : "#637b85",
-            pointColor : "#dbba34",
-            pointStrokeColor : "#637b85",
-            data : [65,59,90,81,30,56]
-        }
-        ]
-    }
-    var canvas = document.getElementById("departments");
-    var ctx = canvas.getContext("2d");
-    new Chart(ctx).Radar(data, options);
+data = {
+    datasets: [{
+        data: [20, 40, 30, 60],
+        backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+    }],
+
+    labels: [
+        'X',
+        'Y',
+        'Z',
+        'B'
+
+    ]
+};
+
+var ctz = document.getElementById("expenditure");
+var expenditure = new Chart(ctz, {
+    data: data,
+    type: 'polarArea'
+});
 
 
 
